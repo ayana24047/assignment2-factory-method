39018ef760b38733e3be05a1f47507d55d5cc37b
ayana <ayana.marhabat@mail.ru>
2025-09-28 14:28:02 +0500

    Initial commit: Factory Method assignment

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 // Client
 public class Main {
     public static void main(String[] args) {
-        // Создаем BMW
+
         CarFactory bmwFactory = new BMWFactory();
         Car bmw = bmwFactory.buildCar();
         System.out.println("Car built: " + bmw.getBrand());
         System.out.println("------------------");
 
-        // Создаем Toyota
         CarFactory toyotaFactory = new ToyotaFactory();
         Car toyota = toyotaFactory.buildCar();
         System.out.println("Car built: " + toyota.getBrand());
